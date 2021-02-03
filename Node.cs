3757dea211cf705712ec4b8af88b23f1ba014429
MechaDragonX <theofficialrv@gmail.com>
2021-02-02 16:24:34 -0800

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhiteBoard_2._2
+{
+    class Node
+    {
+        public int Value;
+        public Node Left;
+        public Node Right;
+
+        public Node(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhiteBoard_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -10, -3, 0, 5, 9 };
+            ConvertSortedArraytoBST(array);
+        }
+
+        private static void ConvertSortedArraytoBST(int[] array)
+        {
+            int median = 0;
+            if (array.Length % 2 != 0)
+            {
+                median = array[array.Length / 2];
+            }
+            int[] leftArray = array.Take(array.Length / 2).ToArray();
+            int[] rightArray = array.Skip(array.Length / 2 + 1).ToArray();
+
+            Node tree = new Node(median);
+            Node current = tree;
+            for (int i = leftArray.Length - 1; i >= 0; i--)
+            {
+                current.Left = new Node(leftArray[i]);
+                current = current.Left;
+            }
+            current = tree;
+            foreach (int item in rightArray)
+            {
+                current.Right = new Node(item);
+                current = current.Right;
+            }
+        }
+    }
+}
